6bea80a530032874cbc784fca97db156561cbf32
winnyace <winnyace@skiff.com>
2023-10-28 12:50:41 +0300

    idk bro

--- file_io/main.c
+++ file_io/main.c
@@ -4,12 +4,12 @@ int main()
 {
     FILE *file;
 
-    file = fopen("testfile", "w+");
+    file = fopen("test", "w+");
     fprintf(file, "Hello, testfile!\n");
     fprintf(file, "This string was written from a C program!");
     fclose(file);
 
-    file = fopen("testfile", "r+");
+    file = fopen("test", "r+");
     char str[255];
     while (fgets(str, sizeof(str), file) != NULL) {
         printf("%s", str);
