1ff2b8191fc75643d02189ba16d0d3ac994b2e9b
winnyace <winnyace@skiff.com>
2023-11-27 11:11:43 +0200

    comment about something

--- leftpad_meme/main.c
+++ leftpad_meme/main.c
@@ -6,6 +6,7 @@ static char result[MAX_STRING_LENGTH];
 
 const char* leftpad(char *src, char *fill, unsigned n)
 {
+    // function improved thanks to the tips from abraker. thank you
     if (!(strlen(src) + n * strlen(fill) <= MAX_STRING_LENGTH && strlen(src) != 0))
     {
         printf("\n\nERROR: The source string was null or the amount you wanted to pad to the left surpassed the maximum string length for the result (which is 512 characters long). Returning the string unmodified\n\n");
@@ -16,6 +17,7 @@ const char* leftpad(char *src, char *fill, unsigned n)
     {
         memset(result, '\0', sizeof(result));
     }
+
     for (unsigned i = 0; i < n; i++)
     {
         strcat(result, fill);
@@ -27,6 +29,6 @@ const char* leftpad(char *src, char *fill, unsigned n)
 int main()
 {
     char str[MAX_STRING_LENGTH];
-    strcpy(str, leftpad("test", " ", 100));
+    strcpy(str, leftpad("This is a test", " ", 10));
     printf("%s", str);
 }
